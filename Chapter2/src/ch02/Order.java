a92a32cab72703ea201e1241fc905a495f171372
Minsoo-Lee <minsoo.james1101@gmail.com>
2021-06-09 01:02:53 +0900

    ~Chapter 02.04

---
+++ Chapter2/src/ch02/Order.java
@@ -0,0 +1,11 @@
+package ch02;
+
+public class Order {
+
+    // 멤버 변수 (member variable)
+    int orderId;
+    String buyerId;
+    String sellerId;
+    int productId;
+    String orderDate;
+}
---
+++ Chapter2/src/ch02/Student.java
@@ -0,0 +1,11 @@
+package ch02;
+
+public class Student {
+
+    // 멤버 변수 (member variable)
+    int studentNumber;
+    String studentName;
+    int majorCode;
+    String majorName;
+    int grade;
+}
