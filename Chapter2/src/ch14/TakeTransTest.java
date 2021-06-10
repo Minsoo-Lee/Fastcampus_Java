d359a1d862783f248c8da2d829a248a139a89749
Minsoo-Lee <minsoo.james1101@gmail.com>
2021-06-10 12:10:19 +0900

    ~Chapter 02.15

---
+++ Chapter2/src/ch14/TakeTransTest.java
@@ -0,0 +1,24 @@
+package ch14;
+
+public class TakeTransTest {
+    public static void main(String[] args) {
+        Student studentJ = new Student("James", 5000);
+        Student studentT = new Student("Thmomas", 5000);
+
+        Bus bus100 = new Bus(100);
+        Bus bus500 = new Bus(500);
+
+        studentJ.takeBus(bus100);
+
+        Subway greenSubway = new Subway (2);
+        studentT.takeSubay(greenSubway);
+
+        studentJ.showInfo();
+        studentT.showInfo();
+
+        bus100.showBusInfo();
+        greenSubway.showBusInfo();
+
+        bus500.showBusInfo();
+    }
+}
