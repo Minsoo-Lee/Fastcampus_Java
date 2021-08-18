68420609d4f959bd45b4d9f90dd515c106b28ae8
Minsoo-Lee <minsoo.james1101@gmail.com>
2021-08-18 21:50:08 +0900

    08.17 / ~Part 03.02

--- Part2/Chapter8/src/ch10/Counting.java
+++ Part2/Chapter8/src/ch10/Counting.java
@@ -2,7 +2,7 @@ package ch10;
 
 public class Counting {
     public static void main(String[] args) {
-        int[] people = {55, 40, 27, 99, 76, 81, 29, 31, 33, 62} ;
+        int[] people = {55, 40, 27, 99, 76, 81, 29, 31, 33, 62};
         int[] ages = new int[10];
 
         for (int i=0; i<people.length; i++) {
